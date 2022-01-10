05d45bb82c175808a77992a14299517862d9edb7
Denzel <dbartolaba123@gmail.com>
2022-01-10 12:41:00 -0800

    display function

--- families/families.js
+++ families/families.js
@@ -14,20 +14,41 @@ logoutButton.addEventListener('click', () => {
     logout();
 });
 
-function displayFamilies() {
+function displayFamilies(families) {
     // fetch families from supabase
+    familiesEl.textContent = '';
 
     // clear out the familiesEl
 
     for (let family of families) {
         // create three elements for each family, one for the whole family, one to hold the name, and one to hold the bunnies
-        
+        const familyEl = document.createElement('div');
+        const nameEl = document.createElement('p');
+        const bunniesEl = document.createElement('div');
         // add the bunnies css class to the bunnies el, and family css class to the family el
-
+        bunniesEl.classList.add('bunnies');
+        familyEl.classList.add('family');
         // put the family name in the name element
-
+        nameEl.textContent = family.name;
         // for each of this family's bunnies
 
+        for (let bunny of family.fuzzy_bunnies) {
+            const bunnyEl = document.createElement('div');
+
+            bunnyEl.classList.add('bunny');
+            bunnyEl.textContent = bunny.name;
+
+            bunnyEl.addEventListener('click', async() => {
+                await deleteBunny(bunny.id);
+
+                const changeFamilies = await getFamilies();
+
+                displayFamilies(changeFamilies);
+            });
+            bunniesEl.append(bunnyEl);
+        }
+        familyEl.append(nameEl, bunniesEl);
+        familiesEl.append(familyEl);
 
         // make an element with the css class 'bunny', and put the bunny's name in the text content
         
