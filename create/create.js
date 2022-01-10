dcbc9ae9cb1f6d872d0157f05f11a5f13489cee9
Denzel <dbartolaba123@gmail.com>
2022-01-10 12:48:08 -0800

    Still need to add bunnies

--- create/create.js
+++ create/create.js
@@ -20,7 +20,7 @@ form.addEventListener('submit', async e => {
     // use createBunny to create a bunny with this name and family id
     await createBunny({
         name: name,
-        family: family
+        family_id: family
 
     });
     
